27555cd34ec2e89b534bb272640b96bfd8000a38
dkrymlov <91798748+dkrymlov@users.noreply.github.com>
2023-11-13 16:29:50 +0200

    Initial Commit

---
+++ VideoCallTest/VideoCallTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCallTestApp.swift
+//  VideoCallTest
+//
+//  Created by Даниил Крымлов on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoCallTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
